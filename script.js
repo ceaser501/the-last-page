305d2e25062dd3a2f852b24ae34a25514de90f59
Taesu.Kim <90tskim@gmail.com>
2025-07-18 03:01:58 +0900

    배경 꾸미기

--- script.js
+++ script.js
@@ -42,6 +42,96 @@ let row = 0;
 function generateRow() {
   if (pointer >= mediaList.length) return;
 
+  /*
+  if (row === 1) {
+    const flowerSticker = document.createElement("img");
+    const flowerSticker2 = document.createElement("img");
+    flowerSticker.src = "./data/flower.png";
+    flowerSticker.alt = "flower Sticker";
+    flowerSticker.style.position = "absolute";
+    flowerSticker.style.top = "530px";
+    flowerSticker.style.left = "calc(25% - 60px)";
+    flowerSticker.style.width = "130px";
+    flowerSticker.style.transform = "rotate(10deg) translateY(-1px)";
+    flowerSticker.style.zIndex = "6";
+    flowerSticker.style.pointerEvents = "none";
+
+    flowerSticker2.src = "./data/flower.png";
+    flowerSticker2.alt = "flower Sticker";
+    flowerSticker2.style.position = "absolute";
+    flowerSticker2.style.top = "530px";
+    flowerSticker2.style.left = "calc(23% - 60px)";
+    flowerSticker2.style.width = "130px";
+    flowerSticker2.style.transform = "rotate(-6deg) translateY(-1px)";
+    flowerSticker2.style.zIndex = "6";
+    flowerSticker2.style.pointerEvents = "none";
+
+    document.body.appendChild(flowerSticker);
+    document.body.appendChild(flowerSticker2);
+  }
+    */
+
+  if (row === 2) {
+    const flowerSticker = document.createElement("img");
+    flowerSticker.src = "./data/flower2.png";
+    flowerSticker.alt = "flower Sticker";
+    flowerSticker.style.position = "absolute";
+    flowerSticker.style.top = "740px";
+    flowerSticker.style.left = "calc(22% - 10px)";
+    flowerSticker.style.width = "130px";
+    flowerSticker.style.transform = "rotate(-10deg) translateY(-20px)";
+    flowerSticker.style.zIndex = "6";
+    flowerSticker.style.pointerEvents = "none";
+
+    document.body.appendChild(flowerSticker);
+  }
+
+  if (row === 3) {
+    const heartSticker = document.createElement("img");
+    heartSticker.src = "./data/heart.png";
+    heartSticker.alt = "heart Sticker";
+    heartSticker.style.position = "absolute";
+    heartSticker.style.top = "890px";
+    heartSticker.style.left = "calc(71% - 60px)";
+    heartSticker.style.width = "130px";
+    heartSticker.style.transform = "rotate(0deg) translateY(-20px)";
+    heartSticker.style.zIndex = "6";
+    heartSticker.style.pointerEvents = "none";
+
+    const heartSticker2 = document.createElement("img");
+    heartSticker2.src = "./data/heart.png";
+    heartSticker2.alt = "heart Sticker";
+    heartSticker2.style.position = "absolute";
+    heartSticker2.style.top = "870px";
+    heartSticker2.style.left = "calc(78% - 70px)";
+    heartSticker2.style.width = "130px";
+    heartSticker2.style.transform = "rotate(-12deg) translateY(-20px)";
+    heartSticker2.style.zIndex = "6";
+    heartSticker2.style.pointerEvents = "none";
+
+    document.body.appendChild(heartSticker);
+    document.body.appendChild(heartSticker2);
+  }
+
+  if (row === 4) {
+    const dateTape = document.createElement("div");
+    dateTape.className = "date-tape";
+    dateTape.textContent = "2024.09.28 ~ 2026.04.12";
+    dateTape.style.background = "#f2d9c6dc";
+    dateTape.style.opacity = "0.8";
+    dateTape.style.position = "absolute";
+    dateTape.style.right = "500px";
+    dateTape.style.margin = "10px auto -60px auto";
+    dateTape.style.zIndex = "5";
+    dateTape.style.fontFamily = "'Gloria Hallelujah', cursive";
+    dateTape.style.fontSize = "22px";
+    dateTape.style.fontWeight = "bold";
+    dateTape.style.color = "#222";
+    dateTape.style.transform = "translateX(180px) rotate(-2deg)";
+    dateTape.style.maxWidth = "fit-content";
+    wrapper.appendChild(dateTape);
+  }
+
   const imagesPerRow = row % 2 === 0 ? 7 : 5;
   const rowWrapper = document.createElement("div");
   rowWrapper.className = "garland-row";
@@ -56,6 +146,7 @@ function generateRow() {
   rowWrapper.appendChild(rope);
 
   const currentMedia = mediaList.slice(pointer, pointer + imagesPerRow);
+  let prevDoubleTape = false; // 이전 테이프가 2개로 붙었다면, 다음엔 2개짜리가 못나옴
 
   currentMedia.forEach((media, j) => {
     const i = pointer + j;
@@ -71,16 +162,15 @@ function generateRow() {
     let order = j;
     if (row % 2 === 1) order = imagesPerRow - 1 - j;
 
-    const totalWidth = row % 2 === 0 ? 1400 : 1300;
-    const spacing = totalWidth / (imagesPerRow + 1);
-
-    const x = spacing * (order + 1) + (row % 2 === 0 ? 0 : -30);
+    const spacing = 180;
+    const totalRowWidth = spacing * imagesPerRow;
+    const x = `calc(50% - ${totalRowWidth / 2}px + ${spacing * order}px)`;
     const y =
       row % 2 === 0
         ? 50 * Math.sin((Math.PI * j) / (imagesPerRow - 1)) + 80
         : 40 + 50 * j;
 
-    photo.style.left = `${x}px`;
+    photo.style.left = x;
     photo.style.top = `${y}px`;
     photo.style.setProperty("--rotate", `${rotate}deg`);
 
@@ -97,7 +187,6 @@ function generateRow() {
       const thumbnail = document.createElement("img");
       thumbnail.className = "photo-img";
 
-      // 동영상에서 썸네일 자동 추출
       const videoForThumb = document.createElement("video");
       videoForThumb.src = media.src;
       videoForThumb.muted = true;
@@ -106,7 +195,7 @@ function generateRow() {
 
       videoForThumb.addEventListener("loadeddata", () => {
         const canvas = document.createElement("canvas");
-        canvas.width = 160; // 사진 사이즈에 맞춰서
+        canvas.width = 160;
         canvas.height = 150;
         const ctx = canvas.getContext("2d");
         ctx.drawImage(videoForThumb, 0, 0, canvas.width, canvas.height);
@@ -130,10 +219,40 @@ function generateRow() {
     const caption = document.createElement("figcaption");
     caption.innerText = `추억 ${i + 1}`;
 
-    const tape = document.createElement("div");
-    tape.className = "tape";
+    // 테이프 배치 로직
 
-    photo.appendChild(tape);
+    if (!prevDoubleTape && Math.random() < 0.4) {
+      // 40% 확률 - 대각선 테이프 2개
+      prevDoubleTape = true;
+
+      // 우측 상단
+      const tape1 = document.createElement("div");
+      tape1.className = "tape diagonal";
+      tape1.style.top = "0px";
+      tape1.style.left = "130px";
+      tape1.style.transform = "rotate(45deg)";
+      tape1.style.width = "70px";
+      tape1.style.height = "20px";
+      photo.appendChild(tape1);
+
+      // 좌측 하단
+      const tape2 = document.createElement("div");
+      tape2.className = "tape diagonal";
+      tape2.style.top = "190px";
+      tape2.style.left = "-20px";
+      tape2.style.transform = "rotate(45deg)";
+      tape2.style.width = "60px";
+      tape2.style.height = "16px";
+      photo.appendChild(tape2);
+    } else {
+      // 60% 확률 - 상단 중앙 테이프 1개
+      prevDoubleTape = false;
+
+      const tape = document.createElement("div");
+      tape.className = "tape";
+      photo.appendChild(tape);
+    }
+
     photo.appendChild(mediaNode);
     photo.appendChild(caption);
     rowWrapper.appendChild(photo);
